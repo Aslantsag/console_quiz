be624e4f51b05dd7350225b80c59039d7138afb4
Aslan <aslantsag@mail.ru>
2021-03-09 21:24:28 +0300

    master

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 # name = input("Enter your name: ")
 
 with open('wlist.txt') as txt:
+    all_quests = 0
+    success = 0
     quiz = txt.read().split("\n")
     for item in quiz:
         if item[0] == 'Q':
             quest = item[2:]
-            print(quest)
+            print(quest+"\n")
             r_ans = 0
             ans_num = 1
-            success = 0
             count = quiz.index(item) + 1
-            try:
-                while quiz[count][0] == 'L':
-                    if quiz[count][2] == '+':
-                        r_ans = ans_num
-                        ans_text = quiz[count][3:]
-                    else:
-                        ans_text = quiz[count][2:]
-
-                    print(f"{ans_num} - {ans_text}")
-                    ans_num += 1
-                    count += 1
-
-                ans = int(input(">>> "))
-                
-                if ans == r_ans:
-                    success += 1
-                    print("Правильно, молодец!")
+            all_quests += 1
+            while count < len(quiz) and quiz[count][0] == 'L':
+                if quiz[count][2] == '+':
+                    r_ans = ans_num
+                    ans_text = quiz[count][3:]
                 else:
-                    print("Неправильно!")
-            except:
-                print("Тест окончен")
+                    ans_text = quiz[count][2:]
+
+                print(f"{ans_num} - {ans_text}")
+                ans_num += 1
+                count += 1
+
+            ans = int(input(">>> "))
+            
+            if ans == r_ans:
+                success += 1
+                print("Правильно, молодец!")
+            else:
+                print("Неправильно!")
+            print("\n")
+                
+    print(f"Тест окончен! Правильно: {success}, Неправильно: {all_quests - success}, Всего: {all_quests}")
